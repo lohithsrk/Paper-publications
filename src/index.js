51d6d998d32147e811560a2166573ccf5b19d886
lohithsrk <srklohith05@gmail.com>
2023-01-15 18:06:02 +0530

    chNGES

--- src/index.js
+++ src/index.js
@@ -3,24 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './redux/store';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const queryClient = new QueryClient()
 
 root.render(
-  <QueryClientProvider client={queryClient}>
-
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </QueryClientProvider>
-
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
--- src/routes/User.route.jsx
+++ src/routes/User.route.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate, Navigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-
-import { validateUser } from '../axios/auth.axios';
 
 const UserRoute = () => {
 	const { user } = useSelector((state) => ({ ...state }));
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const { isLoading, error, data } = useQuery({
-		queryKey: ['login'],
-		queryFn: () => validateUser(user.token).then((res) => res.json())
-	});
-	console.log("🚀 ~ file: User.route.jsx:18 ~ UserRoute ~ data", data)
-	if (user && user.token) {
-		if (error) {
-			dispatch({ type: 'LOGOUT_USER', payload: null });
-			toast.error('Please login to continue');
-			navigate('/login');
-		}
-		if (!isLoading) {
-			if (!data.token.length) {
-				dispatch({ type: 'LOGOUT_USER', payload: null });
-				navigate('/login');
-			}
-		}
-	}
-
 	return user && user.token.length ? <Outlet /> : <Navigate to='login' />;
 };
 
