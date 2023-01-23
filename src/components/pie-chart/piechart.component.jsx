4dc7c0318a63a4d8481a3d4045499508f7d3f505
Akash2k20 <akashlearning2020@gmail.com>
2023-01-23 20:07:04 +0530

    BetterPie

--- src/components/pie-chart/piechart.component.jsx
+++ src/components/pie-chart/piechart.component.jsx
@@ -5,8 +5,18 @@ function Piechart_component() {
   return (
     <div>
       <PieChart
-        
-        animate= "true"
+        label={({ dataEntry }) => `${Math.round(dataEntry.percentage)} %`}
+        reveal="40%"
+        animate="true"
+        animationDuration="1000"
+        labelPosition={60}
+      
+        labelStyle={{
+          fontSize: "7px",
+          fontColor: "FFFFFA",
+        }}
+        lineWidth={30}
+        rounded="true"
         data={[
           { title: "One", value: 10, color: "#E38627" },
           { title: "Two", value: 15, color: "#C13C37" },
