665fddd8141ef03869333ada9843b01233bf806d
Akash2k20 <akashlearning2020@gmail.com>
2023-01-23 18:02:07 +0530

    Piechart

--- src/pages/Statistics.page.jsx
+++ src/pages/Statistics.page.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React from "react";
+import Piechart_component from "../components/pie-chart/piechart.component";
 
 const Statistics = () => {
-	return <div>Statistics</div>;
+  return <div>
+	<Piechart_component/>
+  </div>;
 };
 
 export default Statistics;
