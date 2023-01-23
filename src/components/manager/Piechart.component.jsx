937400a93a31a73f9273c108b2af6f6db1a5af74
Akash2k20 <akashlearning2020@gmail.com>
2023-01-23 21:12:02 +0530

    BetterPie1

--- src/components/manager/Piechart.component.jsx
+++ src/components/manager/Piechart.component.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { PieChart } from 'react-minimal-pie-chart';
+import React from "react";
+import { Chart } from "react-google-charts";
 
-function PiechartComponent() {
-	return (
-		<div>
-			<PieChart
-				label={({ dataEntry }) => `${Math.round(dataEntry.percentage)} %`}
-				reveal='40%'
-				animate='true'
-				animationDuration='1000'
-				labelPosition={60}
-				labelStyle={{
-					fontSize: '7px',
-					fontColor: 'FFFFFA'
-				}}
-				lineWidth={30}
-				rounded='true'
-				data={[
-					{ title: 'One', value: 10, color: '#E38627' },
-					{ title: 'Two', value: 15, color: '#C13C37' },
-					{ title: 'Three', value: 20, color: '#6A2135' }
-				]}
-			/>
-		</div>
-	);
+export const data = [
+  ["Status", "Count"],
+  ["Reviewed", 2],
+  ["Revision", 2],
+  ["Submitted", 2],
+];
+
+export const options = {
+  title: "Analysis",
+  pieHole: 0,
+  is3D: true,
+};
+
+function Piechart() {
+  return (
+    <Chart
+      chartType="PieChart"
+      width="110%"
+      height="120%"
+      data={data}
+      options={options}
+    />
+  );
 }
 
-export default PiechartComponent;
+export default Piechart;
